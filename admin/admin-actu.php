ddb81d5bdfd17d2129712e433a95a2faf37323e5
boulanger <olivier.dww@gmail.com>
2022-01-13 14:58:30 +0100

    edit actu

--- admin/admin-actu.php
+++ admin/admin-actu.php
@@ -3,14 +3,31 @@
 $actuDb = require __DIR__ . '/../database/models/databaseActu.php';
 
 $_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-    $id = $_GET['id'] ?? ''; 
+$id = $_GET['id'] ?? '';
+$selectAll = $_GET['select'] ?? '';
+$editOne = $_GET['edit'] ?? '';
+$article = [];
 
-    if ($id) {
-        $article = $actuDb->fetchAllArticle($id);
-        $idArticle = $article['id'];
-        $title = $article['title'];
-        $visibility = $article['viibility'];
-    }
+if ($selectAll) {
+    $article = $actuDb->fetchAllArticle($id);
+}
+
+if ($editOne) {
+    $oneActu = $actuDb->fetchArticle($editOne);
+    echo "<pre>";
+    var_dump($oneActu);
+    echo "</pre>";
+    $idArticle = $oneActu['id'];
+    $title = $oneActu['title'];
+    $visibility = $oneActu['visibility'];
+}
+// echo "<pre>";
+// var_dump($article);
+// echo "</pre>";
+// die();
+// $idArticle = $article['id'];
+// $title = $article['title'];
+// $visibility = $article['visibility'];
 
 ?>
 
@@ -21,8 +38,7 @@ $_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="css/admin-actu.css">
     <title>FC Busnes - Page Admin Actu</title>
 </head>
@@ -31,7 +47,7 @@ $_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     <div>
         <div class="admin">
             <h1>Page Admin Actu</h1>
-            <label for="">Title:</label><input type="text" id="titleB">
+            <label for="">Title:</label><input type="text" id="titleB" value="<?= $title ?? '' ?>">
             <label for="">Image:</label><input type="text" id="imgB">
             <label for="">Résumé Equipe1:</label><input type="text" id="texte1B">
             <label for="">Score Equipe1: </label><input type="text" id="score1B" placeholder="Score final: ">
@@ -43,7 +59,7 @@ $_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
             <label for="">Visibilité:</label><input type="text" id="visibilityB" placeholder=" mettre true pour afficher, mettre false si on n'affiche pas l'article">
             <div class="button">
                 <button id="insert">INSERT</button>
-                <button name="id">SELECT ALL</button>
+                <a href="./admin-actu.php?select=ok" name="id">SELECT ALL</a>
             </div>
         </div>
         <table class="table">
@@ -57,14 +73,14 @@ $_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
                 </tr>
             </thead>
             <tbody>
-                <?php foreach($article as $a) : ?>
-                <tr>
-                    <th><?= $a['id'] ?></th>
-                    <th><?= $a['title'] ?></th>
-                    <th><?= $a['visibility'] ?></th>
-                    <th><button class="edit">Editer</button></th>
-                    <th><button class="delete">Delete</button></th>
-                </tr>
+                <?php foreach ($article as $a) : ?>
+                    <tr>
+                        <th><?= $a['id'] ?></th>
+                        <th><?= $a['title'] ?></th>
+                        <th><?= $a['visibility'] ?></th>
+                        <th><a href="./admin-actu.php?edit=<?= $a['id'] ?>" name="id">EDITER</a></th>
+                        <th><button class="delete">Delete</button></th>
+                    </tr>
                 <?php endforeach; ?>
             </tbody>
         </table>
@@ -72,6 +88,6 @@ $_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
             <button class="button2"><a href="index_admin.php" class="return">Retour à l'accueil</a></button>
         </div>
     </div>
-        <!-- <script src="js/admin-actu.js" type="module"></script> -->
+    <!-- <script src="js/admin-actu.js" type="module"></script> -->
 
 </html>
